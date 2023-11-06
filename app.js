c6bec67be7d9e6da5c2c89af04b0c19d7a114230
Raul593 <calvimonteraul@gmail.com>
2023-11-06 20:55:26 -0300

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+const PORT = process.env.PORT ?? 3000;
+ 
+app.listen(PORT, ()=>{
+    console.log('Servidor funcionando ' + PORT);
 });
 
 app.get('/', (req,res)=>{
